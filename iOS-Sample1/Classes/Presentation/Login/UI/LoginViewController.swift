657ee6c290840af15a97dbcccbf1cf180ae25ff0
maguhiro <maguhiro@gmail.com>
2019-06-15 00:35:41 +0900

    メアドとパスワード共に入力時のみログインボタンがタップできるように制御

--- iOS-Sample1/Classes/Presentation/Login/UI/LoginViewController.swift
+++ iOS-Sample1/Classes/Presentation/Login/UI/LoginViewController.swift
@@ -1,7 +1,41 @@
+import RxCocoa
+import RxSwift
 import UIKit
 
 final class LoginViewController: UIViewController {
+  @IBOutlet private var emailTextField: UITextField!
+  @IBOutlet private var passwordTextField: UITextField!
+  @IBOutlet private var loginButton: UIButton!
+
+  private let disposeBag = DisposeBag()
+
   override func viewDidLoad() {
     super.viewDidLoad()
+    setupBinding()
+  }
+}
+
+// MARK: Viewのレイアウト
+
+private extension LoginViewController {
+  func setupBinding() {
+    let emailObservable = emailTextField.rx.text.orEmpty.asDriver().asObservable()
+    let passwordObservable = passwordTextField.rx.text.orEmpty.asDriver().asObservable()
+
+    Observable
+      .combineLatest(emailObservable, passwordObservable)
+      .map { email, password -> Bool in
+        !email.isEmpty && !password.isEmpty
+      }
+      .bind(to: loginButton.rx.isEnabled)
+      .disposed(by: disposeBag)
+  }
+}
+
+// MARK: ユーザーアクション
+
+private extension LoginViewController {
+  @IBAction func tapLoginButton(_: UIButton) {
+    print("clicked Log In Button")
   }
 }
