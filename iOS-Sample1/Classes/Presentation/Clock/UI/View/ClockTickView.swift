97d430af52ff66b3a223151fbc4cb41e60476c67
maguhiro <maguhiro@gmail.com>
2019-06-16 00:30:47 +0900

    ドラッグの動きに合わせて回転するよう対応

--- iOS-Sample1/Classes/Presentation/Clock/UI/View/ClockTickView.swift
+++ iOS-Sample1/Classes/Presentation/Clock/UI/View/ClockTickView.swift
@@ -7,7 +7,10 @@ final class ClockTickView: UIView {
 
   private let imageViewList = [UIImageView(), UIImageView(), UIImageView(), UIImageView(), UIImageView(), UIImageView()]
 
+  private var startTime: CFTimeInterval?
+  private var firstTapPoint: CGPoint?
   private var pauseFlg = false
+  private var pauseDegree: Double?
 
   override init(frame: CGRect) {
     super.init(frame: frame)
@@ -35,8 +38,8 @@ final class ClockTickView: UIView {
 
     let circle = UIBezierPath(arcCenter: rect.center,
                               radius: radius,
-                              startAngle: calculateAngle(degree: 0.0),
-                              endAngle: calculateAngle(degree: 360.0),
+                              startAngle: calculateRadian(degree: 0.0),
+                              endAngle: calculateRadian(degree: 360.0),
                               clockwise: true)
     let color = UIColor.white
     color.setStroke()
@@ -65,7 +68,9 @@ private extension ClockTickView {
   func panAction(_ sender: UIPanGestureRecognizer) {
     switch sender.state {
     case .began:
-      if !isGestureArea(sender.location(in: self)) {
+      let firstTapPoint = sender.location(in: self)
+      self.firstTapPoint = firstTapPoint
+      if !isGestureArea(firstTapPoint) {
         return
       }
 
@@ -77,6 +82,13 @@ private extension ClockTickView {
       if pauseFlg {
         pauseFlg = false
         restartAnimation()
+      }
+    case .changed:
+      if let firstTapPoint = firstTapPoint {
+        let currentPoint = sender.location(in: self)
+        let currentDegree = calculateDegree(from: bounds.center, to: currentPoint)
+        let firstDegree = calculateDegree(from: bounds.center, to: firstTapPoint)
+        moveTransform(degree: currentDegree - firstDegree)
       }
     default:
       break
@@ -102,7 +114,7 @@ private extension ClockTickView {
   }
 
   func calculateRadian(from centerPoint: CGPoint, to targetPoint: CGPoint) -> Double {
-    return atan2(Double(targetPoint.y - centerPoint.y), Double(targetPoint.x - targetPoint.y))
+    return atan2(Double(targetPoint.y - centerPoint.y), Double(targetPoint.x - centerPoint.y))
   }
 
   func calculateDegree(from centerPoint: CGPoint, to targetPoint: CGPoint) -> Double {
@@ -118,17 +130,18 @@ private extension ClockTickView {
     return CGPoint(x: x, y: y)
   }
 
-  func calculateAngle(degree: CGFloat) -> CGFloat {
-    return CGFloat(Double.pi) * 2 * degree / 360.0
-  }
-
   func calculateRadian(degree: CGFloat) -> CGFloat {
     return degree * CGFloat(Double.pi) / 180.0
+  }
+
+  func calculateDegree(radian: CGFloat) -> CGFloat {
+    return radian * 180.0 / CGFloat(Double.pi)
   }
 }
 
 extension ClockTickView {
   func startAnimation() {
+    startTime = layer.convertTime(CACurrentMediaTime(), from: nil)
     startCircleAnimation()
     imageViewList.forEach { startImageAnimation($0) }
   }
@@ -160,8 +173,19 @@ extension ClockTickView {
 
 private extension ClockTickView {
   func stopAnimation() {
+    pauseDegree = getCurrentRotateDegree()
     stopAnimation(view: self)
     imageViewList.forEach { stopAnimation(view: $0) }
+  }
+
+  func getCurrentRotateDegree() -> Double? {
+    if let transform = layer.presentation()?.transform {
+      let degree = calculateDegree(radian: atan2(transform.m12, transform.m11))
+      log.i(degree)
+      return Double(degree)
+    }
+
+    return nil
   }
 
   func stopAnimation(view: UIView) {
@@ -169,6 +193,42 @@ private extension ClockTickView {
     let pausedTime = layer.convertTime(CACurrentMediaTime(), from: nil)
     layer.timeOffset = pausedTime
     layer.speed = 0.0
+  }
+}
+
+private extension ClockTickView {
+  func moveTransform(degree: Double) {
+    guard let pauseDegree = pauseDegree else {
+      return
+    }
+
+    let tmpDegree: Double = {
+      if degree < 0 {
+        return degree + 360.0
+      }
+      return degree
+    }()
+
+    moveTransform(view: self, degree: tmpDegree + pauseDegree)
+    imageViewList.forEach { moveTransform(view: $0, degree: tmpDegree + pauseDegree) }
+  }
+
+  func moveTransform(view: UIView, degree: Double) {
+    let layer = view.layer
+
+    let tmpDegree: Double = {
+      if degree < 0 {
+        return degree + 360.0
+      }
+      if 360 <= degree {
+        return degree - 360.0
+      }
+      return degree
+    }()
+
+    let percentage = tmpDegree / 360.0 * 100
+    let diffTime = Double(percentage) / rotationDuration
+    layer.timeOffset = startTime! + diffTime
   }
 }
 
